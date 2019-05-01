b872ddbc95dae922556199eaabe20f6419bc4dd8
Monique Warrington <niquewarrington@gmail.com>
2019-05-01 14:23:46 +1200

    AdminSuperUser Created. Consumer, Company, Product groups created

--- Home/models.py
+++ Home/models.py
@@ -9,6 +9,15 @@ class Administrator(models.Model):
     phone_number = models.IntegerField(max_length=25)
     username = models.CharField(max_length=50)
 
+    # to make the class more readable when outputted
+    def __str__(self):
+        return '%s %s %s %s %s %s' % (self.admin_ID,
+                                      self.first_name,
+                                      self.last_name,
+                                      self.email_address,
+                                      self.phone_number,
+                                      self.username)
+
 
 class Consumer(models.Model):
     consumer_ID = models.AutoField(primary_key=True)
@@ -18,14 +27,35 @@ class Consumer(models.Model):
     phone_number = models.IntegerField(max_length=25)
     username = models.CharField(max_length=50)
 
+    # to make the class more readable when outputted
+    def __str__(self):
+        return '%s %s %s %s %s %s' % (self.consumer_ID,
+                                      self.first_name,
+                                      self.last_name,
+                                      self.email_address,
+                                      self.phone_number,
+                                      self.username)
+
 
 class Product(models.Model):
     product_id = models.AutoField(primary_key=True)
     product_name = models.CharField(max_length=100)
     product_url = models.URLField(max_length=200)
 
+    # to make the class more readable when outputted
+    def __str__(self):
+        return '%s %s %s' % (self.product_id,
+                             self.product_name,
+                             self.product_url)
+
 
 class Company(models.Model):
     company_id = models.AutoField(primary_key=True)
     company_name = models.CharField(max_length=100)
     username = models.CharField(max_length=50)
+
+    # to make the class more readable when outputted
+    def __str__(self):
+        return '%s %s %s' % (self.company_id,
+                             self.company_name,
+                             self.username)
