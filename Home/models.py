277f533347eb3425798f9cfb8bc491f1d8746d2f
Monique Warrington <niquewarrington@gmail.com>
2019-05-01 13:55:19 +1200

    Models Initialized

--- Home/models.py
+++ Home/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Administrator(models.Model):
+    admin_ID = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email_address = models.EmailField(max_length=100)
+    phone_number = models.IntegerField(max_length=25)
+    username = models.CharField(max_length=50)
+
+
+class Consumer(models.Model):
+    consumer_ID = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email_address = models.EmailField(max_length=100)
+    phone_number = models.IntegerField(max_length=25)
+    username = models.CharField(max_length=50)
+
+
+class Product(models.Model):
+    product_id = models.AutoField(primary_key=True)
+    product_name = models.CharField(max_length=100)
+    product_url = models.URLField(max_length=200)
+
+
+class Company(models.Model):
+    company_id = models.AutoField(primary_key=True)
+    company_name = models.CharField(max_length=100)
+    username = models.CharField(max_length=50)
