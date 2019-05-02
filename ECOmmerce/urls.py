c233b34bfcef6b77dddebbcc15394c353a8daeeb
AnthonyMarzan <anthony.m@hotmail.co.nz>
2019-05-02 17:04:36 +1200

    renamed a file, redirected to main page
    Renamed Home file to home as it couldnt find homepage
    Redirected http://127.0.0.1:8000 so that it automatically goes to http://127.0.0.1:8000/home

--- ECOmmerce/urls.py
+++ ECOmmerce/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('home/', include('home.urls')),
     path('admin/', admin.site.urls),
+]
+
+urlpatterns += [
+    path('', RedirectView.as_view(url='/home/')),
 ]
